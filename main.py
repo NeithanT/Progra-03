d0ce7374b2661be5c1e5575fb589aa8f9a7aba30
Neithan <neithanvarvar@hotmail.com>
2025-06-11 17:35:18 -0600

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Main File for the Project
+# To execute use in terminal:
+# python3 main.py in unix based systems
+# python main.py in windows
+
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    root.title("window")
+    root.geometry("300x300")
+
+    root.mainloop()
